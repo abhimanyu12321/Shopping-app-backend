ff661539e43b5ae24c4190483377d71b57dbb3d4
Abhimanyu <122272179+abhimanyu12321@users.noreply.github.com>
2023-08-25 13:42:45 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -8,13 +8,15 @@ const sendToken = (user, statusCode, res) => {
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
         httpOnly: true,
+        sameSite: "none",
+        secure:true
     }
 
     res.status(statusCode).cookie('token', token, option).json({
         success: true,
         user,
         token,
-        sameSite: "None",
+      
     })
 }
 
