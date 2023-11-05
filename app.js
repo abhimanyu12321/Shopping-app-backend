931838ef20ff0cb5e2ce465661e189b14666c3d9
Goru <abhiabhi9087@gmail.com>
2023-11-05 18:53:20 +0530

    Logout error fix

--- app.js
+++ app.js
@@ -8,24 +8,24 @@ const fileUpload = require("express-fileupload");
 
 
 
-
+// Basic Application Middlewares
 app.use(cookieParser())
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json())
 app.use(fileUpload());
-
-
-//Routes imports
 app.use("*", cors({
     origin: true,
     credentials: true,
 
 }))
+
+//Routes imports
 const product = require("./routes/productRoute")
 const user = require("./routes/userRoute")
 const order = require("./routes/orderRoutes")
 const payment = require("./routes/paymentRoute");
+// Route Middlewares
 app.use("/api/v1", product)
 app.use("/api/v1", user)
 app.use("/api/v1", order)
--- controllers/userController.js
+++ controllers/userController.js
@@ -59,7 +59,9 @@ exports.logout = catchAsyncErrors(async (req, res, next) => {
 
     res.cookie("token", null, {
         expires: new Date(Date.now()),
-        httpOnly: true
+        httpOnly: true,
+        sameSite: "none",
+        secure: true,
     })
 
     res.status(200).json({
